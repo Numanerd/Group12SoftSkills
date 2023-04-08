203fe485213230d3f8739a09827248934521cbf7
keremdizdar <keremdizdar@gmail.com>
2023-04-09 00:50:54 +0300

    6. ve 7. user story in step lerin pomları

---
+++ src/test/java/Pages/OrangePom.java
@@ -0,0 +1,39 @@
+package Pages;
+
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrangePom extends Parent {
+
+    public OrangePom() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    @FindBy(name = "username")
+    public WebElement username;
+
+    @FindBy(name = "password")
+    public WebElement password;
+
+    @FindBy(css = "button[type='submit']")
+    public WebElement loginButton;
+
+    @FindBy(linkText = "Admin")
+    public WebElement adminButton;
+
+    @FindBy(css = "[class='oxd-icon bi-plus oxd-button-icon']")
+    public WebElement addButton;
+
+    @FindBy(xpath = "(//*[@class='oxd-input oxd-input--active'])[2]")
+    public WebElement username2;
+
+    @FindBy(xpath = "//*[text()='Already exists']")
+    public WebElement alreadyExists;
+
+    @FindBy(xpath = "//*[text()='Should be at least 5 characters']")
+    public WebElement fiveCharacters;
+
+
+}
